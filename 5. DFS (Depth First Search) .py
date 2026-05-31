cc000561f10ad36d13d36ebcaa39ea9207ae5e64
Kilau Girda Alifia <250306012.mhs@uinmataram.ac.id>
2026-06-01 04:02:33 +0800

    Reformat DFS algorithm with consistent indentation

--- 5. DFS (Depth First Search) .py
+++ 5. DFS (Depth First Search) .py
@@ -1,6 +1,6 @@
 # 5. DFS (Depth First Search) 
 # algoritma pencarian yang menjelajahi node secara mendalam sebelum melanjutkan ke node lain. 
-# Selesai praktikum se
+# Selesai praktikum s
 
 from collections import deque
 
