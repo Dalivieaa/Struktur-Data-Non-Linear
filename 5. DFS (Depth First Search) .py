aa5902c6f263506707f7f960d0f095da30e5326e
Kilau Girda Alifia <250306012.mhs@uinmataram.ac.id>
2026-06-01 03:59:51 +0800

    Update 5. DFS (Depth First Search) .py

--- 5. DFS (Depth First Search) .py
+++ 5. DFS (Depth First Search) .py
@@ -1,6 +1,6 @@
 # 5. DFS (Depth First Search) 
 # algoritma pencarian yang menjelajahi node secara mendalam sebelum melanjutkan ke node lain. 
-
+# Selesai praktikum semester 2
 
 from collections import deque
 
