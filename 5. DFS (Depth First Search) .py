6ea6dd210effff40ce85d6e1acd3524d15df9007
Kilau Girda Alifia <250306012.mhs@uinmataram.ac.id>
2026-06-01 04:02:54 +0800

    Reformat DFS algorithm code and comments

--- 5. DFS (Depth First Search) .py
+++ 5. DFS (Depth First Search) .py
@@ -1,6 +1,6 @@
 # 5. DFS (Depth First Search) 
 # algoritma pencarian yang menjelajahi node secara mendalam sebelum melanjutkan ke node lain. 
-# Selesai praktikum s
+# Selesai praktikum 
 
 from collections import deque
 
